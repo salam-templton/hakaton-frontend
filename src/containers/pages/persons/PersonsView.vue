6e3755d8c635f4ed20a1a67bf91daea6ee6a0e1b
Dmitry Sal <dimasalam@yandex.ru>
2025-11-07 01:44:54 +0700

    Вынес loader без пропсов в ui

--- src/containers/pages/persons/PersonsView.vue
+++ src/containers/pages/persons/PersonsView.vue
@@ -2,8 +2,8 @@
 import MainLayout from '@/containers/layout/main-layout/MainLayout.vue'
 import { usePersonListStore } from '@/stores/person-list/person-list-store.tsx'
 import { storeToRefs } from 'pinia'
-import { LoadingOutlined } from '@ant-design/icons-vue'
 import PersonsTable from '@/containers/pages/persons/cmp/PersonsTable.vue'
+import BaseLoader from '@/shared/ui/loaders/BaseLoader.vue'
 
 const store = usePersonListStore()
 
@@ -19,13 +19,11 @@ const { loadPersons } = store
       <a-button @click="loadPersons">Загрузить список пользователей</a-button>
 
       <template v-if="loading">
-        <a-flex vertical style="height: 100%; padding-top: 200px">
-          <LoadingOutlined style="font-size: 150px" />
-        </a-flex>
+        <BaseLoader />
       </template>
 
       <template v-else>
-        <PersonsTable/>
+        <PersonsTable />
       </template>
     </template>
   </MainLayout>
---
+++ src/shared/ui/loaders/BaseLoader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { LoadingOutlined } from '@ant-design/icons-vue'
+</script>
+
+<template>
+  <a-flex vertical style="height: 100%; padding-top: 200px">
+    <LoadingOutlined style="font-size: 150px" />
+  </a-flex>
+</template>
